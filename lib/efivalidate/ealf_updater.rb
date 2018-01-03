5bd423c79ab9c1d4ea4f874944aac209d41ac6b0
Rick Mark <rickmark@dropbox.com>
2018-01-03 14:57:23 -0800

    Expand number of baselines

---
+++ lib/efivalidate/ealf_updater.rb
@@ -0,0 +1,7 @@
+module EFIValidate
+   class EALFUpdater
+     UPDATE_INDEX = 'https://swscan.apple.com/content/catalogs/others/index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz'
+
+     
+   end
+end
