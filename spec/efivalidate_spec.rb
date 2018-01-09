26fa656b14e44eda9451e60cdabbc8dc4e202ce9
Rick Mark <rickmark@dropbox.com>
2018-01-09 09:31:36 -0800

    Sec core fixkup as released

--- spec/efivalidate_spec.rb
+++ spec/efivalidate_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe EFIValidate do
     expect(EFIValidate::VERSION).not_to be nil
   end
 
-  it 'works on a correct firmware' do
+  it 'works on a correct firmware with SEC_CORE fixup' do
     path = File.join(__dir__, 'fixtures/MBP142.88Z.0167.B00.1708080034.0.ealf')
     parser = EFIValidate::EALFParser.read(path)
 
@@ -13,6 +13,25 @@ RSpec.describe EFIValidate do
     expect(parser.header.ealf_size).to eq File.size(path)
 
     firmware_path = File.join(__dir__, 'fixtures/MBP142_0167_B00.fd')
+    validator = EFIValidate::EFIValidator.new(parser, firmware_path)
+
+    validator.validate
+
+    validator.errors.each do |error|
+      puts error
+    end
+
+    expect(validator.valid?).to be_truthy
+  end
+
+  it 'works on a correct firmware without SEC_CORE' do
+    path = File.join(__dir__, 'fixtures/MBP114.88Z.0177.B00.1708080033.0.ealf')
+    parser = EFIValidate::EALFParser.read(path)
+
+    expect(parser.rows.count).to eq 188
+    expect(parser.header.ealf_size).to eq File.size(path)
+
+    firmware_path = File.join(__dir__, 'fixtures/MBP114_0177_B00.fd')
     validator = EFIValidate::EFIValidator.new(parser, firmware_path)
 
     validator.validate
