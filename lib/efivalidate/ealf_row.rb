cd5e86db9448f989501f14114b12668139b437ed
Rick Mark <rickmark@dropbox.com>
2018-01-17 23:07:26 -0800

    Dynamic mapping of efi table rows to data file

--- lib/efivalidate/ealf_row.rb
+++ lib/efivalidate/ealf_row.rb
@@ -1,4 +1,5 @@
 require 'uuidtools'
+require 'yaml'
 
 module EFIValidate
   # A class that represents a region of an EFI firmware with a valid hash for the region
@@ -40,27 +41,17 @@ module EFIValidate
     end
   end
 
-  ROW_GUIDS = { "ef7f23e1-7ba0-a64a-baea-33edff15ba3f" => 'BIOS_EMPTY_SPACE',
-                "d954937a-6804-4a44-81ce-0bf617d890df" => 'EFI_FLASH_FILE_SYSTEM_VOLUME',
-                "8c1b00bd-716a-7b48-a14f-0c2a2dcf7a5d" => 'APPLE_IMMUTABLE_FIRMWARE_VOLUME',
-                "78e58c8c-3d8a-1c4f-9935-896185c32dd3" => 'EFI_FIRMWARE_FILE_SYSTEM_2_VOLUME',
-                "a980b9e3-e35f-e548-9b92-2798385a9027" => 'EMPTY_SHMOO_FIRMWARE_VOLUME',
-                "484c38ef-0cab-544b-8ed9-0710ad500c0f" => 'FIELD_SHMOO_FIRMWARE_VOLUME',
-                "0a369089-370e-2245-a8a7-a55041013deb" => 'DALE_SHMOO_FIRMWARE_VOLUME',
-                "97213d15-bd29-dc44-ac59-887f70e41a6b" => 'MICROCODE_FIRMWARE_VOLUME',
-                "8d2bf1ff-9676-8b4c-a985-2747075b4f50" => 'EFI_SYSTEM_NNRAM_FIRMWARE_VOLUME',
-                "096de3c3-9482-974b-a857-d5288fe33e28" => 'BIOS_IDENTIFIER',
-                "b57d69bb-60ed-ac46-8754-7580b8b27ed0" => 'APPLE_SEC_VOLUMES_FILE',
-                "2e06a01b-79c7-8245-8566-336ae8f78f09" => 'SEC_CORE',
-                "17706906-2e5f-225e-ad94-5816399c720a" => 'APPLE_ROM_MANIFEST',
-                "a3b9f5ce-6d47-7f49-9fdc-e98143e0422c" => 'AMI_NVRAM_FILE',
-                "24465000-598a-eb4e-bd0f-6b36e96128e0" => 'PHOENIX_NVRAM_FIRMWARE_VOLUME',
-                "153d2197-29bd-44dc-ac59-887f70e41a6b" => 'INTEL_MICROCODE',
-                "fff12b8d-7696-4c8b-a985-2747075b4f50" => 'NVRAM',
-                "04adeead-61ff-4d31-b6ba-64f8bf901f5a" => 'BOOT_VOLUME',
-                "ffffffff-ffff-ffff-ffff-ffffffffffff" => 'EFI_BLOCK_PADDING_REGION'}
+  def self.load_guids
+    apple_guids = YAML.load_file(File.join(__dir__, 'apple_guids.yaml'))
 
-  ROW_GUIDS.each do |key, value|
-    const_set(value, UUIDTools::UUID.parse(key))
+    puts apple_guids.inspect
+
+    const_set(:ROW_GUIDS, apple_guids['guid_types'])
+
+    apple_guids['guid_types'].each do |key, value|
+      const_set(value.to_sym, key)
+    end
   end
+
+  load_guids
 end
